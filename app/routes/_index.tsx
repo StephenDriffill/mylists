c0ff92fd055fc5789df6239246b667a45d42047a
Stephen Driffill <sdriffill@hotmail.co.uk>
2024-02-28 15:26:09 +0000

    Improved dnd behaviour and styling.

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,10 +1,14 @@
+import { DragDropContext, Draggable, Droppable } from '@hello-pangea/dnd';
 import {
   Autocomplete,
+  Box,
   List,
   ListItem,
+  ListItemText,
   TextField,
   Typography,
 } from '@mui/material';
+import { json } from '@remix-run/node';
 import * as React from 'react';
 
 import { Page } from '~/components';
@@ -17,7 +21,47 @@ type Film = (typeof films)[0];
 const MINIMUM_SEARCH_LENGTH = 3;
 const MAX_OPTIONS = 10;
 
+function reorder<T>(list: T[], startIndex: number, endIndex: number) {
+  const result = Array.from(list); // clone the list
+  const [removed] = result.splice(startIndex, 1);
+  result.splice(endIndex, 0, removed);
+
+  return result;
+}
+
+function getListStyle(isDraggingOver: boolean) {
+  return {
+    // background: isDraggingOver ? 'lightblue' : undefined,
+    // padding: grid,
+  };
+}
+
+function getListItemStyle(isDragging: boolean, draggableStyle: any) {
+  return {
+    userSelect: 'none',
+    // padding: grid * 2,
+    // margin: `0 0 ${grid}px 0`,
+    // bgcolor: isDragging ? 'text.secondary' : undefined,
+
+    // styles we need to apply on draggables
+    ...draggableStyle,
+  };
+}
+
+function getTypographyStyle(isDragging: boolean) {
+  return {
+    bgcolor: isDragging ? 'text.secondary' : undefined,
+    borderRadius: 1,
+    paddingY: 0.5,
+    paddingX: 0.5,
+  };
+}
+
 export const meta = () => [{ title: 'MYLISTS' }];
+
+export const loader = async () => {
+  return json({});
+};
 
 export default function Index() {
   const [watchlist, setWatchList] = React.useState<Film['label'][]>(
@@ -33,32 +77,78 @@ export default function Index() {
 
   return (
     <Page>
-      <List>
-        {watchlist.map((movie, index) => (
-          <ListItem
-            disablePadding
-            key={index}
-            sx={{
-              display: 'list-item',
-              listStyle: 'decimal',
-              ml: 3,
-              paddingY: 1,
-              pl: 2,
-              fontSize: 18,
-              color: 'text.secondary',
-            }}
-          >
-            <Typography
-              sx={{
-                fontSize: 20,
-                color: 'text.primary',
-              }}
-            >
-              {movie}
-            </Typography>
-          </ListItem>
-        ))}
-      </List>
+      <Box sx={{ display: 'flex' }}>
+        <List>
+          {watchlist.map((movie, index) => (
+            <ListItem disablePadding key={movie} sx={{ height: 40 }}>
+              <ListItemText
+                sx={{
+                  fontSize: 18,
+                  color: 'text.secondary',
+                }}
+              >
+                {index + 1}
+              </ListItemText>
+            </ListItem>
+          ))}
+        </List>
+        <DragDropContext
+          onDragEnd={(result) => {
+            setWatchList((prev) => {
+              // dropped outside the list
+              if (result.destination === null) {
+                return prev;
+              }
+              return reorder(
+                prev,
+                result.source.index,
+                result.destination.index,
+              );
+            });
+          }}
+        >
+          <Droppable droppableId="droppable">
+            {(provided, snapshot) => (
+              <List
+                {...provided.droppableProps}
+                ref={provided.innerRef}
+                sx={getListStyle(snapshot.isDraggingOver)}
+              >
+                {watchlist.map((movie, index) => (
+                  <Draggable draggableId={movie} index={index} key={movie}>
+                    {(provided, snapshot) => (
+                      <ListItem
+                        ref={provided.innerRef}
+                        {...provided.draggableProps}
+                        {...provided.dragHandleProps}
+                        sx={{
+                          // fontSize: 20,
+                          height: 40,
+                          color: 'text.primary',
+                          ...getListItemStyle(
+                            snapshot.isDragging,
+                            provided.draggableProps.style,
+                          ),
+                        }}
+                      >
+                        <ListItemText disableTypography>
+                          <Typography
+                            component="span"
+                            noWrap
+                            sx={getTypographyStyle(snapshot.isDragging)}
+                          >
+                            {movie}
+                          </Typography>
+                        </ListItemText>
+                      </ListItem>
+                    )}
+                  </Draggable>
+                ))}
+              </List>
+            )}
+          </Droppable>
+        </DragDropContext>
+      </Box>
       <Autocomplete
         autoHighlight
         clearOnBlur={false}
